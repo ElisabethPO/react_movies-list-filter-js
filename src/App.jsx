eb9666d33517d557f179ac451bf92f3c81e02385
Elisabeth Polska <polskaelizaveta@gmail.com>
2025-03-20 16:53:25 +0200

    new solution

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,8 @@ export const App = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
-                onChange={event => {
-                  setQuery(event.target.value);
+                onChange={changeEvent => {
+                  setQuery(changeEvent.target.value);
                 }}
               />
             </div>
